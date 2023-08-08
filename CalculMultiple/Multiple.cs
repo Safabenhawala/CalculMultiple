80eae2d02030561b666d5288717371825284192b
Safa ben Hawala <safabenhawala@gmail.com>
2023-08-08 23:26:06 +0100

    update Function Multiple

---
+++ CalculMultiple/Multiple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculMultiple
+{
+    class Multiple
+    {
+        public string Multiple_3(int n) {
+
+            if (n % 3 == 0) {
+                return "n est une multiple du 3";
+            }
+            return "n n'est pas de multiple du 3"; 
+        }
+
+        public string Multiple_5(int n)
+        {
+
+            if (n % 5 == 0)
+            {
+                return "n est une multiple du 5";
+            }
+            return "n n'est pas de multiple du 5";
+        }
+        public string Multiple_7(int n)
+        {
+
+            if (n % 7 == 0)
+            {
+                return "n est une multiple du 7";
+            }
+            return "n n'est pas de multiple du 7";
+        }
+    }
+}
--- CalculMultiple/Program.cs
+++ CalculMultiple/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace CalculMultiple
     {
         static void Main(string[] args)
         {
+
+            Multiple multiple = new Multiple();
+            string cheminFichier = $"C:/Users/safa/Documents/test_Essai/Resultat_C.txt"; 
+
+
+            string restultat = multiple.Multiple_3(3);
+
+            Console.WriteLine(restultat);
+
+            // Écrire le résultat dans le fichier
+            using (StreamWriter writer = new StreamWriter(cheminFichier, true)) // Le deuxième argument "true" indique d'ajouter du contenu sans écraser le fichier existant
+            {
+                writer.WriteLine(restultat);
+            }
+
+
         }
     }
 }
